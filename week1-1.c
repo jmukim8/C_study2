809f2528940139181660fe05b0d3bb4f1e3affe5
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-09-16 17:42:26 +0900

    Create week1-1.c

---
+++ week1-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int num[ ] = {5, 2, 6, 7, 8, 9, 4, 3, 1};
+  int page[10] = {123, 57, 67, 86};
+
+  for(int i = 0; i < 9; i++)
+    {
+      printf("%d ", num[i]);
+    }
+
+   for(int j = 0; j < 10; j++)
+    {
+      printf("%d ", page[j]);
+    }
+  return 0;
+}
+페이지10
