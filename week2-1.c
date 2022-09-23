53110fa76e7d2c60848106252b78cd12dca372bc
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-09-23 17:40:35 +0900

    Create week2-1.c

---
+++ week2-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  char ch1;
+
+  char str[10] = {0,};
+  printf("hello쓰셈:");
+  scanf("%s", str);
+  int i = 0;
+
+  while (str[i])
+    {
+      str[i] = str[i] - 32;
+      i++;
+    }
+
+  printf("%s", str);
+  return 0;
+}
+
+33페이지,5문제
