3374a3901cd8a218132dd35738cc6e9d6d807e4a
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-09-30 16:00:35 +0900

    Create week3-1.c

---
+++ week3-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  char ch1;
+  char str[10] = {0};
+  char str[4] = str[0];
+  int i;
+  int j;
+  
+  printf("hello쓰셈:");
+  scanf("%s", str);
+  for (i = 0; i < 5; i++)
+    for (j = 5; j > 0; j--)  
+  return 0;
+}
