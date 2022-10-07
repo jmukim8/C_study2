236d642d2fd93d282c5db1ceb4dfb3eda2609d24
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-10-07 15:37:27 +0900

    Create week3-2.c

---
+++ week3-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main( ) 
+{
+  int a, b;
+  int add, sub, mul, div;
+  int *pa, *pb;
+
+  pa = &a;
+  pb = &b;
+
+  a = 30;
+  b = 55;
+  add = *pa + *pb;
+  printf("add = %d\n", add);
+
+  *pa = 9;
+  b = 3;
+  mul = *pa * *pb;
+  printf("ull = %d\n", mul);
+
+  a = 45;
+  *pb = 5;
+  div = *pa / *pb;
+  printf("div = %d\n", div);
+  
+  return 0;
+}
+44페이지,1
