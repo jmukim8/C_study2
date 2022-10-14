4218de6ed267a7eb7c781211f12386c728aeea13
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-10-14 17:21:42 +0900

    Create week5-3.c

---
+++ week5-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+  int ar[] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+  int *p = ar;
+  int i;
+  int len = sizeof(ar) / sizeof(ar[0]);
+
+  for (i = 0; i < len; i++)
+  {
+     printf("%d %d\n", ar[i], *(ar+i));
+  }
+ 
+  return 0;
+}
+9페이지
