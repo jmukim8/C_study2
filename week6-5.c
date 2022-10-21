cd6f41d37cdcc1f66c552f0109db69f015f1860c
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-10-21 17:53:24 +0900

    Create week6-5.c

---
+++ week6-5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Circle
+  {
+    int x;
+    int y;
+    double r;
+    double area;
+    double length;
+  };
+
+int main() 
+  {
+    struct Circle c;
+    c.x = 0;
+    c.y = 0;
+    c.r = 10;
+    c.area = c.r*c.r*3.14;
+    c.length = 2*c.r*3.14;
+
+    printf("원의 넓이 %lf\n", c.area);
+    printf("원의 둘레 %lf\n", c.length);
+
+    return 0;
+  }
