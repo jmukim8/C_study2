18b0d3a0c8f3aa765c0fd942562e66851e3a9ef8
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-10-28 16:20:13 +0900

    Update week6-5.c

--- week6-5.c
+++ week6-5.c
@@ -2,25 +2,25 @@
 #include <string.h>
 
 struct Circle
-  {
-    int x;
-    int y;
-    double r;
-    double area;
-    double length;
-  };
+{
+  int x;
+  int y;
+  double r;
+  double area;
+  double length;
+};
 
 int main() 
-  {
-    struct Circle c;
-    c.x = 0;
-    c.y = 0;
-    c.r = 10;
-    c.area = c.r*c.r*3.14;
-    c.length = 2*c.r*3.14;
+{
+  struct Circle c;
+  c.x = 0;
+  c.y = 0;
+  c.r = 10;
+  c.area = c.r*c.r*3.14;
+  c.length = 2*c.r*3.14;
 
-    printf("원의 넓이 %lf\n", c.area);
-    printf("원의 둘레 %lf\n", c.length);
+  printf("원의 넓이 %lf\n", c.area);
+  printf("원의 둘레 %lf\n", c.length);
 
-    return 0;
-  }
+  return 0;
+}
