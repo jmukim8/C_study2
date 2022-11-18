546739e56a6d44f7a1c421ec93a1a8b17cc706b9
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-11-18 17:38:37 +0900

    Create week10-1.c

---
+++ week10-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+  int ar[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int i;
+  int *pr;
+
+  printf("%d %d %d\n", sizeof(ar), sizeof(ar[0]), sizeof(int));
+
+  for (i = 0; i < sizeof(ar) / sizeof(ar[0]); i++)
+  {
+    printf("%d \n", ar[i]);
+  }
+
+  pr = (int*)malloc(sizeof(int)*20);
+  for(i = 0; i < 20; i++)
+  {
+    pr[i] = i+1;
+    printf("%d ", pr[i]);
+  }
+  return 0;
+}
+//47쪽
